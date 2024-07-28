698617a5d2a74879f97e18a9f378128637281cc5
leejh7 <777joonho@kookmin.ac.kr>
2024-07-28 18:38:24 +0900

    feat #1 - 비밀번호 암호화 설정
    현재는 빈 껍데기로 비밀번호 암호화 정책 결정 후 추가 작성 필요

--- src/main/java/org/bts/backend/config/SecurityConfig.java
+++ src/main/java/org/bts/backend/config/SecurityConfig.java
@@ -1,2 +1,9 @@
-package org.bts.backend.config;public class SecurityConfig {
+package org.bts.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    // TODO: 패스워드 인코딩 전략 정해서 빈으로 등록시킬 것
 }
