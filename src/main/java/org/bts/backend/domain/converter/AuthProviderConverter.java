3fc9cb75191fa4168f0999b4ca6d2815c670b141
leejh7 <777joonho@kookmin.ac.kr>
2024-07-28 18:34:36 +0900

    feat #1 - 회원 가입 및 로그인 타입 작성
    - Local (자체 로그인), Kakao (카카오 로그인) 타입 작성
    - DB에 저장하고 로드할 때 convert 로직 작성

--- src/main/java/org/bts/backend/domain/converter/AuthProviderConverter.java
+++ src/main/java/org/bts/backend/domain/converter/AuthProviderConverter.java
@@ -1,2 +1,19 @@
-package org.bts.backend.domain.converter;public class AuthProviderConverter {
+package org.bts.backend.domain.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import org.bts.backend.domain.constant.AuthProvider;
+
+@Converter
+public class AuthProviderConverter implements AttributeConverter<AuthProvider, Integer> {
+
+    @Override
+    public Integer convertToDatabaseColumn(AuthProvider authProvider) {
+        return authProvider.getCode();
+    }
+
+    @Override
+    public AuthProvider convertToEntityAttribute(Integer code) {
+        return AuthProvider.of(code);
+    }
 }
