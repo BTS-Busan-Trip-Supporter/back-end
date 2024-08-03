8953794af7159234a7acce1fe56507eca1b11881
minseokey <lms990427@kookmin.ac.kr>
2024-08-03 20:17:38 +0900

    feat: add RefreshToken entity for redis repository

---
+++ src/main/java/org/bts/backend/domain/RefreshToken.java
@@ -0,0 +1,22 @@
+package org.bts.backend.domain;
+
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+@NoArgsConstructor
+@RedisHash(value = "refreshToken", timeToLive = 60 * 60 * 24 * 7)
+@Getter
+public class RefreshToken {
+    @Id
+    private String email;
+    private String refreshTokenId;
+
+    public RefreshToken of (String email, String refreshTokenId){
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.email = email;
+        refreshToken.refreshTokenId = refreshTokenId;
+        return refreshToken;
+    }
+}
--- src/main/java/org/bts/backend/repository/RefreshTokenRedisRepository.java
+++ src/main/java/org/bts/backend/repository/RefreshTokenRedisRepository.java
@@ -1,5 +1,6 @@
 package org.bts.backend.repository;
 
+import org.bts.backend.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
