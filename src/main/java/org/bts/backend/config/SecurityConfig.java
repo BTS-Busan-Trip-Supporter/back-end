dc3b43161672e03bc3f7192712795824d3e320ec
minseokey <lms990427@kookmin.ac.kr>
2024-08-03 10:34:11 +0900

    feat: authentication initial manager

--- src/main/java/org/bts/backend/config/SecurityConfig.java
+++ src/main/java/org/bts/backend/config/SecurityConfig.java
@@ -3,16 +3,24 @@ package org.bts.backend.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+
 public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
     // TODO: 패스워드 인코딩 전략 정해서 빈으로 등록시킬 것
     @Bean
@@ -31,6 +39,14 @@ public class SecurityConfig {
                         .deleteCookies("REFRESHTOKEN"));
                 // 추가로 CORS, JWT 필터 필요
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(provider);
     }
 
 }
