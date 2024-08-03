039a59186b2452c711f39b70ffeb7c88dd82fb43
minseokey <lms990427@kookmin.ac.kr>
2024-08-03 20:09:37 +0900

    feat: redis repository init

---
+++ src/main/java/org/bts/backend/repository/RefreshTokenRedisRepository.java
@@ -0,0 +1,8 @@
+package org.bts.backend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, String>{
+}
