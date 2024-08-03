4e82b61e909dd7a650bbd86e19383f00cb6cf8d0
minseokey <lms990427@kookmin.ac.kr>
2024-08-03 10:00:00 +0900

    feat: initial filterchain

--- src/main/java/org/bts/backend/config/SecurityConfig.java
+++ src/main/java/org/bts/backend/config/SecurityConfig.java
@@ -1,9 +1,36 @@
 package org.bts.backend.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     // TODO: 패스워드 인코딩 전략 정해서 빈으로 등록시킬 것
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                // 우선 모든 요청에 대해 허용하도록 설정
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().permitAll())
+                .sessionManagement(sessionManagement ->
+                                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .logout((logout) -> logout.logoutUrl("/logout")
+                        .deleteCookies("REFRESHTOKEN"));
+                // 추가로 CORS, JWT 필터 필요
+        return http.build();
+    }
+
 }
