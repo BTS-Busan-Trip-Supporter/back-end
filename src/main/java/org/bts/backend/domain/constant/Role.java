368d6480d0767add388cc2a65237e140e750511e
minseokey <lms990427@kookmin.ac.kr>
2024-08-03 18:42:29 +0900

    feat: add role unverified

--- src/main/java/org/bts/backend/domain/constant/Role.java
+++ src/main/java/org/bts/backend/domain/constant/Role.java
@@ -4,6 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum Role {
+
+    // 이메일 인증 전에 부여되는 권한, 소셜로그인에서는 해당 없음
+    ROLE_UNVERIFIED("ROLE_UNVERIFIED"),
+    // 이메일 인증 완료 혹은 소셜로그인시에 부여되는 권한
     ROLE_USER("ROLE_USER"),
     ROLE_ADMIN("ROLE_ADMIN");
 
