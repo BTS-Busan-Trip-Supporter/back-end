3f7f54e7654b238211f9b85057179664a35d7041
minseokey <lms990427@kookmin.ac.kr>
2024-08-05 11:45:33 +0900

    feat: conf cookie provider

---
+++ src/main/java/org/bts/backend/util/CookieProvider.java
@@ -0,0 +1,41 @@
+package org.bts.backend.util;
+
+import jakarta.servlet.http.Cookie;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieProvider {
+
+    @Value("${jwt.refresh_expire}")
+    private String refreshExpire;
+
+    //  RefreshToken 을 쿠키에 저장.
+    public ResponseCookie createRefreshCookie(String refreshToken) {
+        return ResponseCookie.from("refreshToken", refreshToken)
+                .httpOnly(true)
+                .maxAge(Long.parseLong(refreshExpire))
+                .path("/")
+                .build();
+    }
+
+    // 쿠키에 저장된 RefreshToken을 삭제.
+    public ResponseCookie deleteRefreshCookie() {
+        return ResponseCookie.from("refreshToken", null)
+                .httpOnly(true)
+                .maxAge(0)
+                .path("/")
+                .build();
+    }
+
+    // ResponseCookie를 Cookie로 변환.
+    public Cookie of (ResponseCookie responseCookie) {
+        Cookie cookie = new Cookie(responseCookie.getName(), responseCookie.getValue());
+        cookie.setHttpOnly(responseCookie.isHttpOnly());
+        cookie.setMaxAge((int) responseCookie.getMaxAge().getSeconds());
+        cookie.setPath(responseCookie.getPath());
+        return cookie;
+    }
+
+}
