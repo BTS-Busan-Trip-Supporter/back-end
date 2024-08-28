edf6c686a5f857b51b4b0066460f5c276f3d9a77
777joonho <777joonho@kookmin.ac.kr>
2024-08-28 17:53:46 +0900

    feat - TourAPI 응답 객체 작성

---
+++ src/main/java/org/bts/backend/dto/response/tourapi/Header.java
@@ -0,0 +1,8 @@
+package org.bts.backend.dto.response.tourapi;
+
+public record Header(
+    String resultCode,
+    String resultMsg
+) {
+
+}
---
+++ src/main/java/org/bts/backend/dto/response/tourapi/SearchKeywordResponse.java
@@ -0,0 +1,46 @@
+package org.bts.backend.dto.response.tourapi;
+
+import java.util.List;
+
+public record SearchKeywordResponse(
+    Response<SearchKeywordBody> response
+) {
+
+    public record SearchKeywordBody(
+            Items items,
+            int numOfRows,
+            int pageNo,
+            int totalCount
+    ) {
+
+    }
+
+    public record Items(List<Item> item) {
+
+    }
+
+    public record Item(
+            String addr1,
+            String addr2,
+            String areacode,
+            String booktour,
+            String cat1,
+            String cat2,
+            String cat3,
+            String contentid,
+            String contenttypeid,
+            String createdtime,
+            String firstimage,
+            String firstimage2,
+            String cpyrhtDivCd,
+            String mapx,
+            String mapy,
+            String mlevel,
+            String modifiedtime,
+            String sigungucode,
+            String tel,
+            String title
+    ) {
+
+    }
+}
