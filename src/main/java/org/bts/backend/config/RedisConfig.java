0aebe384dc9cf1f804f4cd7b0e03c6a380375ab1
minseokey <lms990427@kookmin.ac.kr>
2024-09-17 19:20:25 +0900

    feat: use single redis

--- src/main/java/org/bts/backend/config/RedisConfig.java
+++ src/main/java/org/bts/backend/config/RedisConfig.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -15,23 +17,23 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @RequiredArgsConstructor
 public class RedisConfig {
     @Value("${redis1.host}")
-    public String REDIS_HOST1;
-    @Value("${redis2.host}")
-    public String REDIS_HOST2;
+    public String REDIS_HOST;
     @Value("${redis1.port}")
-    public int REDIS_PORT1;
-    @Value("${redis2.port}")
-    public int REDIS_PORT2;
+    public int REDIS_PORT;
 
     // 레디스 연결 설정
     @Bean
     public RedisConnectionFactory redisConnectionFactory1() {
-        return new LettuceConnectionFactory(REDIS_HOST1, REDIS_PORT1);
+        RedisStandaloneConfiguration redisConfig =  new RedisStandaloneConfiguration(REDIS_HOST, REDIS_PORT);
+        redisConfig.setDatabase(0);
+        return new LettuceConnectionFactory(redisConfig, LettuceClientConfiguration.defaultConfiguration());
     }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory2() {
-        return new LettuceConnectionFactory(REDIS_HOST2, REDIS_PORT2);
+        RedisStandaloneConfiguration redisConfig =  new RedisStandaloneConfiguration(REDIS_HOST, REDIS_PORT);
+        redisConfig.setDatabase(1);
+        return new LettuceConnectionFactory(redisConfig, LettuceClientConfiguration.defaultConfiguration());
     }
 
     // 레디스 데이터 템플릿 설정, Refresh 토큰 저장용으로 String : Object(RedisToken) 형식으로 설정
