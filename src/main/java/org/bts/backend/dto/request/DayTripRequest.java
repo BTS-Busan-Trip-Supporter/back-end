a0a0221782470bf658369dd7052188429b2aaa70
주노리 <39540595+leejh7@users.noreply.github.com>
2024-09-21 22:12:51 +0900

    Docs/swagger (#32)
    * docs: 관광지 관련 API swagger 문서 작성
    
    * docs: 여행 서비스 관련 API swagger 문서 작성
    
    * docs: request / response dto swagger 작성

--- src/main/java/org/bts/backend/dto/request/DayTripRequest.java
+++ src/main/java/org/bts/backend/dto/request/DayTripRequest.java
@@ -1,13 +1,19 @@
 package org.bts.backend.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 import java.util.List;
 import org.bts.backend.domain.constant.DayTime;
 
+@Schema(description = "당일치기 여행 추천 요청 DTO [참조 figma #4]")
 public record DayTripRequest(
+    @Schema(description = "관광 타입", allowableValues = {"12", "14", "15", "28", "38", "39"})
     String contentTypeId,
+    @Schema(description = "부산광역시의 시/군/구 코드", allowableValues = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"})
     String sigunguCode,
+    @Schema(description = "언제 가시나요", allowableValues = {"MORNING", "MIDNOON", "AFTERNOON", "EVENING"})
     List<DayTime> dayTimes,
+    @Schema(description = "여행 일자")
     LocalDate tourDate
 ) {
 
--- src/main/java/org/bts/backend/dto/response/DayTripResponse.java
+++ src/main/java/org/bts/backend/dto/response/DayTripResponse.java
@@ -1,9 +1,12 @@
 package org.bts.backend.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record DayTripResponse(
     String contentId,
     String contentTypeId,
     String title,
+    @Schema(description = "이미지 url / 빈 공백 문자인 경우 해당하는 이미지 url 없는 것")
     String imageUrl
 ) {
 
